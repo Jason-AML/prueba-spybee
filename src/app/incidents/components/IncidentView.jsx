ad4f00b587e6451458494fb61017026b3f53f3a2
Jason-AML <jaml08@hotmail.com>
2026-06-13 18:44:13 -0500

    refact: se elimino importacion de mapbox, utilizamos componente Image de next

--- src/app/incidents/components/IncidentView.jsx
+++ src/app/incidents/components/IncidentView.jsx
@@ -1,14 +1,10 @@
 "use client";
 
-import { useEffect, useRef } from "react";
-import mapboxgl from "mapbox-gl";
-import "mapbox-gl/dist/mapbox-gl.css";
-import IncidentMap from "./IncidentMap";
-const IncidentView = ({ incident }) => {
-    
+import IncidentMap from "../../components/map/IncidentMap";
+import Image from "next/image";
+import MoreIncident from "./MoreIncident";
 
-  const mapContainer = useRef(null);
-  const map = useRef(null);
+const IncidentView = ({ incident }) => {
   if (!incident) {
     return (
       <div className="p-6 bg-white rounded-lg shadow text-center">
@@ -31,7 +27,7 @@ const IncidentView = ({ incident }) => {
     const colors = {
       low: "bg-green-100 text-green-800",
       medium: "bg-yellow-100 text-yellow-800",
-      high: "bg-orange-100 text-orange-800",
+      high: "bg-red-300 text-orange-800",
       critical: "bg-red-100 text-red-800",
     };
     return colors[priority] || colors.low;
@@ -67,34 +63,6 @@ const IncidentView = ({ incident }) => {
     const i = Math.floor(Math.log(bytes) / Math.log(k));
     return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + " " + sizes[i];
   };
-
-  // Inicializar Mapbox
-  useEffect(() => {
-    if (!incident || !mapContainer.current) return;
-
-    // Set access token
-   mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN;
-
-    // Crear mapa
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: "mapbox://styles/mapbox/streets-v12",
-      center: [incident.coordinates.lng, incident.coordinates.lat],
-      zoom: 15,
-    });
-
-    // Agregar marcador
-    new mapboxgl.Marker({ color: "#EF4444" })
-      .setLngLat([incident.coordinates.lng, incident.coordinates.lat])
-      .addTo(map.current);
-
-    return () => {
-      if (map.current) {
-        map.current.remove();
-      }
-    };
-  }, [incident]);
-
   return (
     <div className="min-h-screen bg-gray-50 p-6">
       <div className="max-w-6xl mx-auto">
@@ -106,30 +74,34 @@ const IncidentView = ({ incident }) => {
                 <span className="text-sm font-semibold text-gray-500">
                   ID: {incident.sequenceId}
                 </span>
-                <span className={`px-3 py-1 rounded-full text-sm font-semibold ${getStatusColor(incident.status)}`}>
+                <span
+                  className={`px-3 py-1 rounded-full text-sm font-semibold ${getStatusColor(incident.status)}`}
+                >
                   {statusLabels[incident.status]}
                 </span>
-                <span className={`px-3 py-1 rounded-full text-sm font-semibold ${getPriorityColor(incident.priority)}`}>
+                <span
+                  className={`px-3 py-1   rounded-full text-sm font-semibold ${getPriorityColor(incident.priority)}`}
+                >
                   {priorityLabels[incident.priority]}
                 </span>
               </div>
               <h1 className="text-3xl font-bold text-gray-900 mb-2">
                 {incident.title}
               </h1>
-              <p className="text-gray-600">
-                {incident.description}
-              </p>
+              <p className="text-gray-600">{incident.description}</p>
             </div>
             {incident.approval && (
-              <div className="ml-4 bg-green-50 border border-green-200 rounded px-3 py-1">
-                <span className="text-sm font-semibold text-green-800">✓ Aprobado</span>
+              <div className="hidden lg:flex ml-2 bg-green-50 border border-green-200 rounded px-3 py-1">
+                <span className="text-sm font-semibold text-green-800">
+                  ✓ Aprobado
+                </span>
               </div>
             )}
           </div>
         </div>
 
         {/* Main Content Grid */}
-        <div className="grid grid-cols-3 gap-6 mb-6">
+        <div className="lg:grid lg:grid-cols-3 gap-6 mb-6 ">
           {/* Left Column - Main Details */}
           <div className="col-span-2">
             {/* Información General */}
@@ -139,7 +111,9 @@ const IncidentView = ({ incident }) => {
               </h2>
               <div className="grid grid-cols-2 gap-6">
                 <div>
-                  <p className="text-sm text-gray-500 mb-1">Tipo de Incidente</p>
+                  <p className="text-sm text-gray-500 mb-1">
+                    Tipo de Incidente
+                  </p>
                   <p className="text-lg font-semibold text-gray-900">
                     {incident.type.name}
                   </p>
@@ -169,30 +143,73 @@ const IncidentView = ({ incident }) => {
 
             {/* Responsables y Observadores */}
             <div className="bg-white rounded-lg shadow p-6 mb-6">
-              <h2 className="text-xl font-bold text-gray-900 mb-4">
-                Equipo
-              </h2>
-              
+              <h2 className="text-xl font-bold text-gray-900 mb-4">Equipo</h2>
+
               {/* Responsable */}
               <div className="mb-6">
                 <p className="text-sm text-gray-500 mb-3 font-semibold">
                   RESPONSABLE PRINCIPAL
                 </p>
-                <div className="flex items-center gap-4">
-                  <img
-                    src={incident.owner.avatarUrl}
-                    alt={incident.owner.name}
-                    className="w-12 h-12 rounded-full"
-                  />
-                  <div>
-                    <p className="font-semibold text-gray-900">
-                      {incident.owner.name}
-                    </p>
-                    <p className="text-sm text-gray-500">
-                      {incident.owner.email}
-                    </p>
+                {incident.owner ? (
+                  <div className="flex flex-col lg:flex-row lg:items-center lg:justify-between gap-4">
+                    <div className="flex items-center gap-4">
+                      <Image
+                        src={
+                          incident.owner?.avatarUrl || "/default-avatar.webp"
+                        }
+                        alt={incident.owner.name}
+                        width={42}
+                        height={42}
+                        className=" rounded-full"
+                      />
+                      <div>
+                        <p className="font-semibold text-gray-900">
+                          {incident.owner.name}
+                        </p>
+                        <p className="text-sm text-gray-500">
+                          {incident.owner.email}
+                        </p>
+                      </div>
+                    </div>
+                    <div className="flex gap-2 flex-wrap md:flex-nowrap">
+                      <button
+                        className="px-3 py-1 flex items-center gap-1 cursor-pointer bg-blue-100 text-blue-700 rounded hover:bg-blue-200 transition-colors text-sm font-medium"
+                        type="button"
+                        title="Contactar"
+                        onClick={() =>
+                          window.open(
+                            `https://wa.me/${incident.owner?.phone}`,
+                            "_blank",
+                          )
+                        }
+                      >
+                        <span className="material-symbols-outlined text-sm">
+                          contact_phone
+                        </span>
+                        Contactar
+                      </button>
+                      <button
+                        className="px-3 py-1 flex items-center gap-1 bg-orange-100 text-orange-700 rounded hover:bg-orange-200 transition-colors text-sm font-medium"
+                        type="button"
+                        title="Reasignar"
+                      >
+                        <span className="material-symbols-outlined">sync</span>
+                        Reasignar
+                      </button>
+                    </div>
                   </div>
-                </div>
+                ) : (
+                  <div>
+                    <h3 className="font-bold">No existe responsable</h3>
+                    <p>Porfavor asigne uno</p>
+                    <button
+                      className="btn bg-yellow-300 border-amber-300 hover:scale-105 transition-transform"
+                      type="button"
+                    >
+                      Asignar
+                    </button>
+                  </div>
+                )}
               </div>
 
               {/* Asignados */}
@@ -203,19 +220,46 @@ const IncidentView = ({ incident }) => {
                   </p>
                   <div className="space-y-3">
                     {incident.assignees.map((assignee) => (
-                      <div key={assignee.id} className="flex items-center gap-4">
-                        <img
-                          src={assignee.avatarUrl}
-                          alt={assignee.name}
-                          className="w-10 h-10 rounded-full"
-                        />
-                        <div className="flex-1">
-                          <p className="font-semibold text-gray-900">
-                            {assignee.name}
-                          </p>
-                          <p className="text-sm text-gray-500">
-                            {assignee.email}
-                          </p>
+                      <div
+                        key={assignee.id}
+                        className="flex flex-col lg:flex-row lg:items-center lg:justify-between gap-4"
+                      >
+                        <div className="flex items-center gap-4">
+                          <Image
+                            src={assignee.avatarUrl}
+                            alt={assignee.name}
+                            width={42}
+                            height={42}
+                            className="rounded-full"
+                          />
+                          <div className="flex-1">
+                            <p className="font-semibold text-gray-900">
+                              {assignee.name}
+                            </p>
+                            <p className="text-sm text-gray-500">
+                              {assignee.email}
+                            </p>
+                          </div>
+                        </div>
+                        <div className="flex gap-2 item-center flex-wrap md:flex-nowrap">
+                          <button
+                            className="px-3 py-1 flex items-center gap-1 cursor-pointer bg-blue-100 text-blue-700 rounded hover:bg-blue-200 transition-colors text-sm font-medium"
+                            type="button"
+                            title="Contactar"
+                          >
+                            <span className="material-symbols-outlined text-sm">
+                              contact_phone
+                            </span>
+                            Contactar
+                          </button>
+                          <button
+                            className="px-3 py-1 flex items-center gap-1 bg-orange-100 text-orange-700 rounded hover:bg-orange-200 transition-colors text-sm font-medium"
+                            type="button"
+                            title="Reasignar"
+                          >
+                            <span className="material-symbols-outlined">sync</span>
+                            Reasignar
+                          </button>
                         </div>
                       </div>
                     ))}
@@ -231,19 +275,46 @@ const IncidentView = ({ incident }) => {
                   </p>
                   <div className="space-y-3">
                     {incident.observers.map((observer) => (
-                      <div key={observer.id} className="flex items-center gap-4">
-                        <img
-                          src={observer.avatarUrl}
-                          alt={observer.name}
-                          className="w-10 h-10 rounded-full"
-                        />
-                        <div className="flex-1">
-                          <p className="font-semibold text-gray-900">
-                            {observer.name}
-                          </p>
-                          <p className="text-sm text-gray-500">
-                            {observer.email}
-                          </p>
+                      <div
+                        key={observer.id}
+                        className="flex flex-col lg:flex-row lg:items-center lg:justify-between gap-4"
+                      >
+                        <div className="flex items-center gap-4">
+                          <Image
+                            src={observer.avatarUrl}
+                            alt={observer.name}
+                            width={42}
+                            height={42}
+                            className="w-10 h-10 rounded-full"
+                          />
+                          <div className="flex-1">
+                            <p className="font-semibold text-gray-900">
+                              {observer.name}
+                            </p>
+                            <p className="text-sm text-gray-500">
+                              {observer.email}
+                            </p>
+                          </div>
+                        </div>
+                        <div className="flex gap-2 flex-wrap md:flex-nowrap">
+                          <button
+                            className="px-3 py-1 flex items-center gap-1 cursor-pointer bg-blue-100 text-blue-700 rounded hover:bg-blue-200 transition-colors text-sm font-medium"
+                            type="button"
+                            title="Contactar"
+                          >
+                            <span className="material-symbols-outlined text-sm">
+                              contact_phone
+                            </span>
+                            Contactar
+                          </button>
+                          <button
+                            className="px-3 py-1 flex items-center gap-1 bg-orange-100 text-orange-700 rounded hover:bg-orange-200 transition-colors text-sm font-medium"
+                            type="button"
+                            title="Reasignar"
+                          >
+                            <span className="material-symbols-outlined">sync</span>
+                            Reasignar
+                          </button>
                         </div>
                       </div>
                     ))}
@@ -263,9 +334,11 @@ const IncidentView = ({ incident }) => {
                     <div key={media.id} className="overflow-hidden rounded-lg">
                       <div className="bg-gray-100 aspect-video flex items-center justify-center overflow-hidden">
                         {media.type === "image" ? (
-                          <img
+                          <Image
                             src={media.url}
                             alt={media.name}
+                            width={42}
+                            height={42}
                             className="w-full h-full object-cover"
                           />
                         ) : (
@@ -299,7 +372,9 @@ const IncidentView = ({ incident }) => {
               <div className="space-y-4">
                 {incident.closingDate && (
                   <div>
-                    <p className="text-sm text-gray-500 mb-1">Fecha de Cierre</p>
+                    <p className="text-sm text-gray-500 mb-1">
+                      Fecha de Cierre
+                    </p>
                     <p className="font-semibold text-gray-900">
                       {formatDate(incident.closingDate)}
                     </p>
@@ -315,6 +390,10 @@ const IncidentView = ({ incident }) => {
                 )}
               </div>
             </div>
+
+            <MoreIncident project={incident.project.id} />
+
+            {/*Seccion mas incidencias*/}
 
             {/* Etiquetas */}
             {incident.tags.length > 0 && (
