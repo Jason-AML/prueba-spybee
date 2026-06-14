f2f33dd22924810e861da0a950bf79cb59fa6290
Jason-AML <jaml08@hotmail.com>
2026-06-14 01:00:02 -0500

    fix: Incident id sin await

--- src/app/(detail-incident)/incidents/[id]/page.js
+++ src/app/(detail-incident)/incidents/[id]/page.js
@@ -7,7 +7,7 @@ import { getUser } from "@/services/auth/auth.server";
 const page = async ({ params }) => {
   const user = await getUser();
   if (!user) redirect("/login");
-  const { id } = params;
+  const { id } = await params;
   //no filtrar, se debe pasar el ID al componente
   // Buscar el incidente en los datos mock
   const incidentData = incidents[0]?.find((incident) => incident.id === id);
