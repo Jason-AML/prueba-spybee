734cf829b77a7f005f9dc052bf567b475b686c9f
Jason-AML <jaml08@hotmail.com>
2026-06-16 04:24:02 -0500

    feat:helper de normalizacion de coordenadas

---
+++ src/utils/coordinate.js
@@ -0,0 +1,17 @@
+// utils/coordinates.js
+
+export const toLatLng = (source) => {
+  if (!source) return null;
+
+  
+  if ("lat" in source && "lng" in source) {
+    return { lat: Number(source.lat), lng: Number(source.lng) };
+  }
+
+ 
+  if ("latitude" in source && "longitude" in source) {
+    return { lat: Number(source.latitude), lng: Number(source.longitude) };
+  }
+
+  return null;
+};
