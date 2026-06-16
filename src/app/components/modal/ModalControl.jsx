e2e84c7afc096df5b8d564022bacb0e1f5470eb8
Jason-AML <jaml08@hotmail.com>
2026-06-16 18:38:49 -0500

    refact: Se cambio el color por amber-300 y se aumento el tamaño del modal max-w-5xl

--- src/app/components/modal/ModalControl.jsx
+++ src/app/components/modal/ModalControl.jsx
@@ -2,21 +2,20 @@
 
 import { useState } from "react";
 
-export const ModalControl = ({ action, children, color = "#00D3BB" }) => {
+export const ModalControl = ({ action, children }) => {
   const [open, setOpen] = useState(false);
 
   return (
     <>
       <button
-        style={{ backgroundColor: color }}
-        className="btn lg:flex items-center gap-2 px-5 py-2.5 text-xs font-bold uppercase tracking-widest rounded-lg transition-transform hover:scale-105"
+        className="btn lg:flex items-center gap-2 bg-amber-300 px-5 py-2.5 text-xs font-bold uppercase tracking-widest rounded-lg transition-transform hover:scale-105"
         onClick={() => setOpen(true)}
       >
         {action}
       </button>
 
       <dialog open={open} className="modal modal-bottom sm:modal-middle">
-        <div className="modal-box">
+        <div className="modal-box max-w-5xl">
           {children}
           <div className="modal-action">
             <button className="btn" onClick={() => setOpen(false)}>
