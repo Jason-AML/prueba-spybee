0cd4b5743628cc9da1a02f4058773ff187d72d8b
Jason-AML <jaml08@hotmail.com>
2026-06-17 03:30:55 -0500

    refactor: Se envia el ID por parametro al componente IncidentView

--- src/app/(detail-incident)/incidents/[id]/page.js
+++ src/app/(detail-incident)/incidents/[id]/page.js
@@ -1,6 +1,5 @@
 import React from "react";
 import IncidentView from "../../incidents/components/IncidentView";
-import { incidents } from "../../../../data/incidents.mock";
 import { redirect } from "next/navigation";
 import { getUser } from "@/services/auth/auth.server";
 
@@ -8,11 +7,7 @@ const page = async ({ params }) => {
   const user = await getUser();
   if (!user) redirect("/login");
   const { id } = await params;
-  //no filtrar, se debe pasar el ID al componente
-  // Buscar el incidente en los datos mock
-  const incidentData = incidents[0]?.find((incident) => incident.id === id);
-
-  return <IncidentView incident={incidentData} />;
+  return <IncidentView idIncident={id} />;
 };
 
 export default page;
