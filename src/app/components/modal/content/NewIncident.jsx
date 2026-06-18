add5f929ba30baed3c0cae5a0571c8f2566e147a
Jason-AML <jaml08@hotmail.com>
2026-06-18 01:50:00 -0500

    style: cambio de color en el boton de envio de formulario

--- src/app/components/modal/content/NewIncident.jsx
+++ src/app/components/modal/content/NewIncident.jsx
@@ -475,7 +475,7 @@ export default function NewIncident() {
           <button
             type="button"
             onClick={handleSubmit}
-            className="px-5 py-2 text-sm font-medium text-white bg-amber-300 rounded-lg hover:bg-amber-500 cursor-pointer transition-colors flex items-center gap-2"
+            className="px-5 py-2 text-sm font-medium text-white bg-blue-600 rounded-lg hover:bg-blue-400 cursor-pointer transition-colors flex items-center gap-2"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
