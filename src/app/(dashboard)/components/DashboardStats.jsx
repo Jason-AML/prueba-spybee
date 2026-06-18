d938b1e269d81bdde1864a6d1c656d5792d79ec4
Jason-AML <jaml08@hotmail.com>
2026-06-18 02:34:45 -0500

    fix: titulo Simpleradar

--- src/app/(dashboard)/components/DashboardStats.jsx
+++ src/app/(dashboard)/components/DashboardStats.jsx
@@ -11,7 +11,7 @@ const DashboardStats = () => {
           <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
             <CustomBarChart title="Prioridad" incidents={filteredValue} />
             <CustomPieChart title="Estado" incidents={filteredValue}/>
-            <SimpleRadarChart title="Prueba por tipo" incidents={filteredValue}/>
+            <SimpleRadarChart title="Tipo" incidents={filteredValue}/>
           </div>
         </section>
 
